c337aa42b2067160253d3189d2c69cfea6442ce5
Shehriyar <AyxanXelilov2009@gmail.com>
2021-06-13 13:42:23 +0400

    Features

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 import logo2 from "./logo2.svg"
 import './App.css'
-import Navbar from './navbar.jsx'
+import Features from "./Features.jsx"
 import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <Navbar/>
+    <Features />
     </div>
   )
 }
